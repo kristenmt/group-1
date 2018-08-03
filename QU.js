6c250557800024eb1612a4f875cbc064cd87c6e4
Kristen Truyol <kristentruyol@Kristens-MacBook-Air.local>
2018-08-03 14:22:31 -0400

    fixed logo link colors

--- QU.js
+++
@@ -1,4 +0,0 @@
-
-let apiKey= "AIzaSyAW9wv7lrHrweuOQI3_khJ0z938IGNqFvM";
-
- var queryUrl = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=" + placeName  + "inputtype=textquery&fields=" + photos + formatted_address +name + rating +opening_hours+"geometry&key=" +apiKey;
