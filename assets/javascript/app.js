c6a30d4950e32468156b6e2c3ffe1fa6d7c0a26d
Chupi <cmera001@fiu.edu>
2018-08-03 14:52:32 -0400

    api functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,5 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    
     // Initialize Firebase
     var config = {
         apiKey: "AIzaSyA_VMGGZF6Q-BW970FUV9RJMBwZ16ETqbA",
@@ -10,38 +9,55 @@ $(document).ready(function(){
         storageBucket: "",
         messagingSenderId: "805611198114"
     };
-    
+
     firebase.initializeApp(config);
-    
+
     // Assign the reference to the database to a variable named 'db'
     var db = firebase.database();
-
     // global variables 
-    var userCuisineChoice = [];
+    var userCuisineChoice = "chinese";
+    var userLat = [];
+    var userLng = [];
     var userPricePrefChoice = [];
-    var userDistancePrefChoice = [];
-
+    var userDistancePrefChoice = 32187;
     function userCuisine(e) {
         e.preventDefault();
-
         var newUserCuisine = $(this).attr("value");
-
         userCuisineChoice.push(newUserCuisine);
-
         console.log("this is you cuisine choice: " + userCuisineChoice);
         console.log("this is the user price range: " + userPricePrefChoice);
         console.log("this is the user distance preference: " + userDistancePrefChoice + " in meters (place api uses meters as length parameter)");
     }
-    function userPricePref (e) {
+    function userZip(e) {
+        // e.preventDefault();
+        var zip = 45377;
+        var queryURL = "http://maps.googleapis.com/maps/api/geocode/json?address=" + zip;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        }).then(function (response) {
+            // console.log(response);
+            var lat = response.results[0].geometry.location.lat;
+            var lng = response.results[0].geometry.location.lng;
+
+
+            userLat.push(lat);
+            userLng.push(lng);
+            console.log(userLat);
+            console.log(userLng);
+
+        });
+
+    }
+    function userPricePref(e) {
         e.preventDefault();
         // grabs user price choice 
         var newUserPricePref = $(this).attr("value");
-
         // Object.freeze(newUserPricePref);
-
         // push to global variable
         userPricePrefChoice.push(newUserPricePref);
-       
+
         // creates local "temporary" object for holding new user price preference
         console.log("this is the user price range: " + userPricePrefChoice);
         // var newUserPricePref = {
@@ -50,43 +66,42 @@ $(document).ready(function(){
         // // upload user price preference to the database
         // db.ref().push(newUserPricePref);
     }
-    
-    
-    function userDistancePref (e) {
+
+
+    function userDistancePref(e) {
         e.preventDefault();
         // grabs user distance choice
         var newUserDistancePrefChoice = $(this).attr("value");
-
         userDistancePrefChoice.push(newUserDistancePrefChoice);
-
         console.log("this is the user distance preference: " + userDistancePrefChoice + " in meters (place api uses meters as length parameter)");
-       
-    }
 
+    }
     function diningSuggestion(e) {
         e.preventDefault();
         //console.log(childSnapshot.val());
-
+        // var userCuisineChoice = "chinese";
+        // var userPricePrefChoice = 4;
+        // var userDistancePrefChoice = 8000;
         //proxy server to avoid CORS Error
         var proxy = "https://cors-anywhere.herokuapp.com/";
         //var queryURL = proxy + "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyB89xW_iR3r4Jxih4DUu_Qz0QePc8sPWfU";
-        var queryURL = proxy + "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?&locationbias=circle:@28.5383°, 81.3792°" + "&radius=" + userDistancePrefChoice + "&type=restaurant&keyword=" + userCuisineChoice + "&price_level=" + userPricePrefChoice + "&key=AIzaSyB89xW_iR3r4Jxih4DUu_Qz0QePc8sPWfU";
-
-        console.log("this is you cuisine choice: " + userCuisineChoice);
-        console.log("this is the user price range: " + userPricePrefChoice);
-        console.log("this is the user distance preference: " + userDistancePrefChoice + " in meters (place api uses meters as length parameter)");
-
+        // var queryURL = proxy + "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=28.5383,81.3792&radius=160000&type=restaurant&keyword=" + userCuisineChoice + "&key=AIzaSyB89xW_iR3r4Jxih4DUu_Qz0QePc8sPWfU"
+        var queryURL = proxy + "https://maps.googleapis.com/maps/api/place/nearbysearch/json?type=restaurant&keyword=" + userCuisineChoice + "&fields=photos,formatted_address,name,opening_hours,rating,price_level&location=" + userLat + "," + userLng + "&radius=" + userDistancePrefChoice + "&key=AIzaSyB89xW_iR3r4Jxih4DUu_Qz0QePc8sPWfU";
+        // console.log("this is you cuisine choice: " + userCuisineChoice);
+        // console.log("this is the user price range: " + userPricePrefChoice);
+        // console.log("this is the user distance preference: " + userDistancePrefChoice + " in meters (place api uses meters as length parameter)");
         $.ajax({
             url: queryURL,
             method: "GET",
         }).then(function (response) {
-            // console.log(response);
+            console.log(response);
         });
     }
-
-    $(".btn-large").on("click", userCuisine);
+    $(".btn-large").on("click", function () {
+        // $("#cuisine-choice-div").hide();
+    });
+    $(".brand-logo").on("click", userZip);
     $(".btn-medium").on("click", userPricePref);
     $(".btn-small").on("click", userDistancePref);
     $(".btn-xlarge").on("click", diningSuggestion);
-
 });
