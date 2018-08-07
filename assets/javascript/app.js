d7f9a580ef6fc40266b1bd0c9eface51a2141ab5
Kristen M Truyol <39445788+kristenmt@users.noreply.github.com>
2018-08-06 20:52:19 -0400

    Revert "fixed minor button details, added preloader"

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -36,14 +36,13 @@ $(document).ready(function () {
     function userZip() {
         var zip = $("#zip").val();
         var queryURL = "http://maps.googleapis.com/maps/api/geocode/json?address=" + zip;
-
+        
         $.ajax({
             url: queryURL,
             method: "GET",
         }).then(function (response) {
             var lat = response.results[0].geometry.location.lat;
             var lng = response.results[0].geometry.location.lng;
-
             userLat.push(lat);
             userLng.push(lng);
         });
@@ -59,32 +58,21 @@ $(document).ready(function () {
         userDistancePrefChoice = $(this).attr("value");
     }
     function diningSuggestion(e) {
-
-        // e.preventDefault();
-
        
-
         //proxy server to avoid CORS Error
         var proxy = "https://cors-anywhere.herokuapp.com/";
         var apiKey = "AIzaSyChlPJLAb8RprOEJSaNR45xofPCnhLRJk8";
         //var queryURL = proxy + "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyB89xW_iR3r4Jxih4DUu_Qz0QePc8sPWfU";
         // var queryURL = proxy + "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=28.5383,81.3792&radius=160000&type=restaurant&keyword=" + userCuisineChoice + "&key=AIzaSyB89xW_iR3r4Jxih4DUu_Qz0QePc8sPWfU"
         var queryURL = proxy + "https://maps.googleapis.com/maps/api/place/nearbysearch/json?type=restaurant&keyword=" + userCuisineChoice + "&fields=photos,formatted_address,name,reviews[],opening_hours,rating,price_level=" + userPricePref + "&location=" + userLat + "," + userLng + "&radius=" + userDistancePrefChoice + "&key=" + apiKey;
-
-
-        var queryURL2 = proxy + "https://maps.googleapis.com/maps/api/place/details/json?placeid=" + sugPlaceId + "&key=" + apiKey;
-        var sugPlaceId = [];
-
-
         
-
         $.ajax({
             url: queryURL,
             method: "GET",
         }).then(function (response) {
             console.log(response);
             ///======push our respone into an array select from 
-            randArray.push(response.results)
+            randArray.push(response.results) 
             // console.log(randArray);
             //======= function to go through the array and select a random result
             var randNumb = Math.floor((Math.random() * response.results.length) + 1);
@@ -133,8 +121,6 @@ $(document).ready(function () {
         var proxy = "https://cors-anywhere.herokuapp.com/";
         var apiKey = "AIzaSyChlPJLAb8RprOEJSaNR45xofPCnhLRJk8";
         var queryURL2 = proxy + "https://maps.googleapis.com/maps/api/place/details/json?placeid=" + sugPlaceId[0] + "&fields=name,review,formatted_phone_number&key=" + apiKey;
-
-      
 
         $.ajax({
             url: queryURL2,
@@ -190,44 +176,9 @@ $(document).ready(function () {
     };
 
 
-
-          //Show the spinLoader
-        // Binds to the global ajax scope
-        $(document).ajaxStart(function () {
-            $("#spinLoader").show();
-        });
-        
-        //Hide the spinLoader
-        $(document).ajaxComplete(function () {
-            $("#spinLoader").hide();
-        });
-
-
-        // document.querySelector("#third-page").style.display = "none";
-        // document.querySelector(".spinLoader").classList.add(".pre-loader-wrapper");
-        // document.querySelector(".pre-loader-wrapper").classList.add(".spinner-layer");
-
-
-        // $("#price-submit").on("click", function (e) {
-        //     e.preventDefault();
-        //     // review();
-        //     $(".spinLoader").show();
-        // });
-        // Hide the spinLoader after ALL elements have loaded
-        // $("#").load(function (e) {
-        //     e.preventDefault();
-        //     // review();
-        //     $("#spinLoader").hide();
-        // });
-        // $(window).load(function() {
-        //     $("#spinLoader").hide();
-        // });
-
-    }
-
     $(".btn-large").on("click", userCuisine);
     //when user adds cuisine and zip code and clicks submit, pg 1 hides, pg 2 shows up
-    $("#submit").on("click", function (e) {
+    $("#submit").on("click", function(e) {
         e.preventDefault();
         userZip();
         // console.log(userCuisineChoice);
@@ -243,7 +194,7 @@ $(document).ready(function () {
     $(".btn-small").on("click", userDistancePref);
     // $(".btn-xlarge").on("click", diningSuggestion);
     //when user clicks button on pg 2 to get suggestions, pg 2 hides, pg 3 shows up
-    $("#price-submit").on("click", function (e) {
+    $("#price-submit").on("click", function(e) {
         e.preventDefault();
         // console.log(userPricePrefChoice);
         // console.log(userDistancePrefChoice);
@@ -252,35 +203,13 @@ $(document).ready(function () {
         $("#second-page").hide();
         // $("#third-page").show();
     });
-
-
-
-
-
-    //Show the review modal
     $("#reviews").on("click", function (e) {
         e.preventDefault();
-        // review();
+        review();
         $("#modalReviews").show();
     });
-    //Hide the reviews modal
-    $("#reviewsBack").on("click", function (e) {
+    $("#map").on("click", function(e) {
         e.preventDefault();
-        // review();
-        $("#modalReviews").hide();
+        // map();
     });
-
-    //Show the Map modal
-    $("#map").on("click", function (e) {
-        e.preventDefault();
-        // review();
-        $("#modalMap").show();
-    });
-    //Hide the Map modal
-    $("#mapBack").on("click", function (e) {
-        e.preventDefault();
-        // review();
-        $("#modalMap").hide();
-    });
-
 });
